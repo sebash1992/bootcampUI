7c20828441cf02aed39c532e19ac2a9006d60975
sebash1992 <sebash1992@gmail.com>
2015-10-02 09:50:46 -0300

    correccion de errores anteriores

--- Topic8/js/index.js
+++ Topic8/js/index.js
@@ -46,10 +46,12 @@ finalApp.controller('TimelineCtrl', function($scope, $http) {
 		    };
 });
 finalApp.controller('TrendDetailsCtrl', ['$scope','$http', '$routeParams', function ($scope, $http, $routeParams) {
+		$scope.saved = localStorage.getItem('blockUsers');
+		$scope.blocks = (localStorage.getItem('blockUsers')!==null) ? JSON.parse($scope.saved) : [];
+		localStorage.setItem('blockUsers', JSON.stringify($scope.blocks));
+
 		var str = $routeParams.trend;
 		var res = str.replace("#", "%23");
-
-		$scope.blockUsers=[{}];
 
 		$http.get('http://localhost:3000/search?q='+res)
 		    .success(function(response) {
@@ -58,8 +60,9 @@ finalApp.controller('TrendDetailsCtrl', ['$scope','$http', '$routeParams', funct
 		$scope.BlockUser = function(twitId) {
 			var user = $scope.twits[twitId].user;
 			console.log(user);
-
-	
+			$scope.blocks.push({user});
+			localStorage.setItem('blockUsers', JSON.stringify($scope.blocks));
+				    	
 		};
 }]);
 
@@ -71,7 +74,6 @@ finalApp.controller('TwitDetailsCtrl', ['$scope', '$routeParams', function ($sco
 finalApp.controller('BlockUsersCtrl', function($scope, $http) {
 		$scope.saved = localStorage.getItem('blockUsers');
 		$scope.blocks = (localStorage.getItem('blockUsers')!==null) ? JSON.parse($scope.saved) : [];
-		console.log($scope.blocks[0].user.name);
 		localStorage.setItem('blockUsers', JSON.stringify($scope.blocks));
 
 		$scope.NoBlockUser = function(userId) {
